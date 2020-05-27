ba8e64e5750177c0d54536d945c801e01d1bfd3f
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-27 17:14:24 +0900

    서버에서 내려준 JSON을 직접 파싱해서 => code를 알아냄.  => 상황에 따라 다른 로그 출력

--- app/src/main/java/kr/co/yhw/apipractice_20200527/LoginActivity.java
+++ app/src/main/java/kr/co/yhw/apipractice_20200527/LoginActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 
 import androidx.databinding.DataBindingUtil;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import kr.co.yhw.apipractice_20200527.databinding.ActivityLoginBinding;
@@ -39,6 +40,19 @@ public class LoginActivity extends BaseActivity {
                     public void onResponse(JSONObject json) {
 
                         Log.d("JSON확인",json.toString());
+
+                        try {
+                            int code = json.getInt("code");
+                            if ( code ==200){
+                                Log.d("분석결과","로그인성공");
+                            }
+                            else{
+                                Log.d("분석결과","로그인실패");
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
                     }
                 });
